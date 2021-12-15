4cf699729857d300908df4beb11a76a9f120ef76
Dane-Jinn <n10917853@qut.edu.au>
2021-12-15 21:47:15 +0900

    dane changing test

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace MajorProjectITD121
         static void Main(string[] args)
         {   
             Console.WriteLine("Hello World!");
+            Console.WriteLine("C#");
             Console.ReadLine();
             
         }
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # MajorProjectITD121
 This is the project for ITD121 
 Hi My name is Dane
-
+I can't see yours
 
 # Hello Dane
