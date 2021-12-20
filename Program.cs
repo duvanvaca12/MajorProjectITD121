02fc9a9b86b0fa1d589c5a31b580348cf31d36b9
Dane-Jinn <n10917853@qut.edu.au>
2021-12-21 00:19:48 +0900

    Adding the program Cs file

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+class Customer
+{
+    public string Name {get; set; }
+    public string Surname {get; set; }
+    public string Phone {get; set; }
+    public string Address {get;set; }
+    public string Email {get; set; } 
+    public int ID {get; set; }
+    public Customer (string name, string surname, string phone, string address, string email)
+    {
+      ID += 1;
+      Name = name;
+      Surname = surname;
+      Phone = phone;
+      Address = address;
+      Email = email;
+    }
+}
+class Product
+{
+    public Customer Name {get; set; }
+    public int Price {get; set; }
+    public int Quality {get; set; }
+    public int ProductID {get; set;}
+    public Product (Customer name, int price, int quality)
+    {
+       //ProductID = Customer ID;
+       Name = name;
+       Price = price;
+       Quality = quality;
+    }
+}
+class Sale
+{
+    public Customer ID;
+    public int ProductID;
+    public Customer quantity;
+    public Sale(Customer id, int productId, Customer quantity)
+    {
+        ID = id;
+
+    }
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Store store = new Store("Ted","Lasso");
+        Console.ReadLine();
+        
+    }
+}
