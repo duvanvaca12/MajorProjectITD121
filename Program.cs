0006b5fc985b721dc56eeb29407ac7f492207c50
Dane-Jinn <n10917853@qut.edu.au>
2021-12-23 02:43:53 +0900

    dane made changes project 4

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 class Customer
 {
     public string Name {get; set; }
@@ -7,27 +7,28 @@ class Customer
     public string Phone {get; set; }
     public string Address {get;set; }
     public string Email {get; set; } 
-    public int ID {get; set; }
-    public Customer (string name, string surname, string phone, string address, string email)
+    public int CustomerID {get; set; }
+    private static int LoyaltyPoints = 100;
+    public Customer (string customername, string surname, string phone, string address, string email)
     {
-      ID += 1;
-      Name = name;
+      CustomerID += 1;
+      Name = customername;
       Surname = surname;
       Phone = phone;
       Address = address;
       Email = email;
     }
 }
-class Product
+class Product 
 {
     public int ProductName {get; set; }
     public int Price {get; set; }
     public int Quantity {get; set; }
     public int ProductID {get; set;}
-    public Product (int name, int price, int quantity)
+    public Product (int productname, int price, int quantity)
     {
-       //ProductID
-       ProductName = name;
+       ProductID += 1;
+       ProductName = productname;
        Price = price;
        Quantity = quantity;
     }
@@ -37,6 +38,8 @@ class Sale
     public int CustomerID;
     public int ProductID;
     public int Quantity;
+    public bool Delivery;
+    public bool SpendLoyalty;
     public Sale(int customerId, int productId, int quantity)
     {
         CustomerID = customerId;
@@ -49,7 +52,18 @@ class Sale
 
 class Store
 {
-
+    private List<Customer> _customer;
+    private List<Product> _product;
+    public Store()
+    {
+        _customer = new List<Customer>();
+        _product = new List<Product>();
+    }
+    public void AddProduct(int name, int price, int quantity)
+    {
+        Product newProduct = new Product(name,price,quantity);
+        _product.Add(newProduct);
+    }
 }
 
 class Program
